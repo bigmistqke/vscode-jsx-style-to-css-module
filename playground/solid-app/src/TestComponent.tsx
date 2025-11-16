cd3830f746866c614a1151e6f90b3f8530fdc276
bigmistqke <vandijckv@gmail.com>
2025-11-16 17:33:20 +0100

    chore: update solid playground

--- playground/solid-app/src/TestComponent.tsx
+++ playground/solid-app/src/TestComponent.tsx
@@ -1,24 +1,63 @@
-import styles from './TestComponent.module.css'
+import type { ComponentProps } from 'solid-js'
 
-function Comp(props: {style: Record<string, any>}){
+function Comp(props: ComponentProps<'div'>) {
   return <></>
 }
 
 export default () => {
   return (
-    <div style={{ 'background-color': 'white', color: 'blue', padding: '10px 20px', 'border-radius': '4px', border: 'none' }}>
-      <Comp style={{ 'background-color': 'white', color: 'blue', padding: '10px 20px', 'border-radius': '4px', border: 'none' }}/>
-      <h1 style={{ 'background-color': 'white', color: 'blue', padding: '10px 20px', 'border-radius': '4px', border: 'none' }}>
+    <div
+      style={{
+        'background-color': 'white',
+        color: 'blue',
+        padding: '10px 20px',
+        'border-radius': '4px',
+        border: 'none',
+      }}
+    >
+      <Comp
+        style={{
+          'background-color': 'white',
+          color: 'blue',
+          padding: '10px 20px',
+          'border-radius': '4px',
+          border: 'none',
+        }}
+      />
+      <h1
+        style={{
+          'background-color': 'white',
+          color: 'blue',
+          padding: '10px 20px',
+          'border-radius': '4px',
+          border: 'none',
+        }}
+      >
         React Test Component
       </h1>
-      <p style={{ 'background-color': 'white', color: 'blue', padding: '10px 20px', 'border-radius': '4px', border: 'none' }}>
-        This is a test component with inline styles for React.
-        Select the component and run "Extract Inline Styles to CSS Module" command.
+      <p
+        style={{
+          'background-color': 'white',
+          color: 'blue',
+          padding: '10px 20px',
+          'border-radius': '4px',
+          border: 'none',
+        }}
+      >
+        This is a test component with inline styles for React. Select the component and run "Extract
+        Inline Styles to CSS Module" command.
       </p>
-      <button style={{ 'background-color': 'white', color: 'blue', padding: '10px 20px', 'border-radius': '4px', border: 'none' }}>
+      <button
+        style={{
+          'background-color': 'white',
+          color: 'blue',
+          padding: '10px 20px',
+          'border-radius': '4px',
+          border: 'none',
+        }}
+      >
         Click Me
       </button>
     </div>
   )
 }
-
